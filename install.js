32d6e822c1211697a18d0c2afbbe5ea7c5eaa80b
kevin3251 <herry3251@gmail.com>
2018-04-10 17:07:15 +0800

    Update install.js

--- install.js
+++ install.js
@@ -6,10 +6,21 @@ const process = require('process');
 let targetAddon = path.join('./addon', process.platform, process.arch, 'v' + process.versions.modules, 'iohook.node');
 let destination = path.join('./build', 'Release', 'iohook.node');
 
-fs.copyFile(targetAddon, destination, (err) => {
+function errorHandle(err){
     if (err) {
         console.log('addon copy error');
         console.error(err || '');
         throw err;
     }
-});
+}
+
+function copyFile(){
+    let version = parseInt(process.version.slice(1,2));
+    if(version == 7){
+        fs.createReadStream(targetAddon).pipe(fs.createWriteStream(destination));
+    }else{
+        fs.copyFile(targetAddon, destination, errorHandle);
+    }
+}
+
+copyFile();
